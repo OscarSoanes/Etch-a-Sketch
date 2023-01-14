23c2541c257ff3ee27c800639684d6c348067de3
Oscar Soanes <oscarsoanes@gmail.com>
2023-01-14 18:06:03 +0000

    user can now choose their own pixel count

--- javascript.js
+++ javascript.js
@@ -1,33 +1,57 @@
-const grid = document.querySelector("#grid");
-const gridWidth = grid.clientWidth
-const gridHeight = grid.clientHeight
 
+function makeGrid(pixels) {
+    const grid = document.querySelector("#grid");
+    const size = grid.clientWidth;
+    for (let y = 0; y < pixels; y++) {
+        // adding row
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.display = "flex"
 
-for (let y = 0; y < 16; y++) {
+        // adding column
+        for (let x = 0; x < pixels; x++) {
+            const column = document.createElement("div");
+            column.classList.add("pixel");
+            column.style.width = `${size / pixels}px`;
+            column.style.height = `${size / pixels}px`;
 
-    // adding row
-    const row = document.createElement("div");
-    row.classList.add("row");
-    row.style.display = "flex"
+            column.style.outline = "1px solid";
+            // column.style.outlineOffset = "1px";
 
-    // adding column
-    for (let x = 0; x < 16; x++) {
-        const column = document.createElement("div");
-        column.classList.add("pixel");
-        column.style.width = `${gridWidth/16}px`;
-        column.style.height = `${gridHeight/16}px`;
-
-        column.style.outline = "1px solid";
-        // column.style.outlineOffset = "1px";
-
-        row.appendChild(column);
+            row.appendChild(column);
+        }
+        grid.appendChild(row);
     }
-
-    grid.appendChild(row);
 }
 
+function clearGrid() {
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach(pixel => pixel.remove());
+}
+
+makeGrid(16);
+
+// hover effect
 const pixels = document.querySelectorAll(".pixel");
 
 pixels.forEach(pixel => pixel.addEventListener('mouseover', function (e) {
-    pixel.classList.add("coloured")
+    pixel.classList.add("coloured");
 }));
+
+// assign grid count
+const btnCount = document.querySelector(".pixelCounter");
+btnCount.addEventListener('click', function (e) {
+    const length = prompt("How many pixels do you want?")
+    
+    if (length < 4) {
+        alert("Pixel count is too little, please enter a different number")
+        return
+    } else if (length > 64) {
+        alert("Pixel count is too high, please enter a different value")
+        return
+    }
+
+    clearGrid()
+    makeGrid(length)
+})
